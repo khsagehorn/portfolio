64ab68dba4af2a723232b33f5bd7aa37ddefc80e
Klaus Sagehorn <klaussagehorn@Klauss-MacBook-Pro.local>
2016-01-02 16:06:44 -0700

    update project html css and jquery

--- application.js
+++ application.js
@@ -1,1 +1,12 @@
-$ ( "button" ) .click ();
+$( document ).ready(function(){
+  $ ( "#from" ).click(function(){
+     $("#fromtext").slideDown("slow");
+    // console.log('clicked');
+  });
+  $ ( "#deal" ).click(function(){
+     $("#dealtext").slideDown("slow");
+     $("#continue").show();
+    // console.log('clicked');
+  });
+
+});
